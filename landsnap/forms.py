31144d643f6fec62751ef029455d56ea031c5329
MeFerdi <ferdinandodhis254@gmail.com>
2025-04-22 00:28:19 +0300

    refactor: remove redundant comments for improved code clarity in UploadForm

--- landsnap/forms.py
+++ landsnap/forms.py
@@ -4,7 +4,6 @@ from .models import ImageUpload
 from .utils.validators import validate_image_size, validate_image_dimensions
 
 class UploadForm(forms.ModelForm):
-    # Add explicit field definitions for more control
     image1 = forms.ImageField(
         label=_('Before Image'),
         help_text=_('Upload the earlier image of the location'),
@@ -31,7 +30,6 @@ class UploadForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Add Bootstrap form-control class to all fields
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'})
 
@@ -40,7 +38,6 @@ class UploadForm(forms.ModelForm):
         image1 = cleaned_data.get('image1')
         image2 = cleaned_data.get('image2')
         
-        # Validate each image individually
         for field_name, image in [('image1', image1), ('image2', image2)]:
             if image:
                 try:
@@ -49,7 +46,6 @@ class UploadForm(forms.ModelForm):
                 except forms.ValidationError as e:
                     self.add_error(field_name, e)
         
-        # Additional validation that requires both images
         if image1 and image2:
             self._validate_image_pair(image1, image2)
         
@@ -59,7 +55,6 @@ class UploadForm(forms.ModelForm):
         """
         Validate that the two images are suitable for comparison
         """
-        # Check if images are too different in size
         size_ratio = image1.size / image2.size
         if size_ratio > 2 or size_ratio < 0.5:
             raise forms.ValidationError(
